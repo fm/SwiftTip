c8e9dd6a4a3639c1b6283897470e7ad0ccb3f1ef
Justin Lauffer <justin.lauffer@me.com>
2015-06-27 20:20:53 -0400

    initial v2 commit

--- SwiftTip/ViewController.swift
+++ SwiftTip/ViewController.swift
@@ -11,19 +11,19 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var billTextField: UITextField!
-    @IBOutlet weak var numberOfGuestsSegmentedControl: UISegmentedControl!
     @IBOutlet weak var tipRateSegmentedControl: UISegmentedControl!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var eachPaysLabel: UILabel!
-    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var numberOfGuests: UIStepper!
+    @IBOutlet weak var guestLabel: UILabel!
     
-    @IBAction func calculateTapped(sender: AnyObject) {
+    func calculateTip() {
         // This code is run each time the Calculate Button is tapped
         
         
         var userInput = billTextField.text as NSString // Get Bill
-        var totalBill: Double = floor(userInput.doubleValue) // Convert String to Float
+        var totalBill: Double = userInput.doubleValue // Convert String to Double
         
         var index: Int = tipRateSegmentedControl.selectedSegmentIndex
         var tipRate: Double = 0.00 // set tip rate to first index
@@ -42,44 +42,20 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         var tip: Double = totalBill * tipRate // calculate the tip
         let roundedTip = Double(round(100*tip)/100) // rounding the tip
-        tipLabel.text = NSString(format: "Tip Amount: $%.2f", roundedTip) as String // Limiting to 2 decimals
+        tipLabel.text = NSString(format: "$%.02f", roundedTip) as String // Limiting to 2 decimals
         
         var total: Double = totalBill + roundedTip // add tip to total
         var roundedTotal = Double(round(100*total)/100) //rounding the total
-        totalLabel.text = NSString(format: "Total Amount: $%.2f", roundedTotal) as String // Limiting to 2 decimals
+        totalLabel.text = NSString(format: "$%.02f", roundedTotal) as String // Limiting to 2 decimals
         
         
-        var guestIndex: Int = numberOfGuestsSegmentedControl.selectedSegmentIndex
-        var guestNumber: Int = 1 // set number of guests to 1 for default
-        
-        // set number of guests according to segment control postion
-        
-        switch guestIndex {
-            
-        case 0:
-                guestNumber = guestIndex + 1
-        case 1:
-                guestNumber = guestIndex + 1
-        case 2:
-                guestNumber = guestIndex + 1
-        case 3:
-                guestNumber = guestIndex + 1
-        case 4:
-                guestNumber = guestIndex + 1
-        case 5:
-                guestNumber = guestIndex + 1
-        default:
-            break
-        
-        }
-
+        var guestNumber = numberOfGuests.value // sets value to stepper
         
         // Debugging
         // println(tipRate) // make sure it selects the right tip amount
-        // println(guestNumber) // make sure it selects right number of guests
-        
+
         var guestTotal: Double = roundedTotal / Double(guestNumber) // divide bill by # of guests
-        eachPaysLabel.text = NSString(format: "Each Guest Pays: $%.2f", guestTotal) as String // Limiting to 2 decimals
+        eachPaysLabel.text = NSString(format: "$%.02f", guestTotal) as String // Limiting to 2 decimals
     }
 
     override func viewDidLoad() {
@@ -88,20 +64,27 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         self.billTextField.delegate = self
 
-        // calculate button border
-        self.calculateButton.layer.cornerRadius = 7
-        self.calculateButton.layer.masksToBounds = true
-        self.calculateButton.layer.borderWidth = 1
-        self.calculateButton.layer.borderColor = UIColor.blueColor().CGColor
           }
+
+    // "\(numberOfGuests.value)"
+    
+    @IBAction func numberGuestChanged(sender: AnyObject) {
+        guestLabel.text = String(format: "%.00f", numberOfGuests.value) // displaying label as text
+        calculateTip()    }
+    
+    @IBAction func totalChanged(sender: AnyObject) {
+        calculateTip()
+    }
     
     
     @IBAction func guestTap(sender: AnyObject) {
         self.view.endEditing(true)
+        calculateTip()
     }
     
     @IBAction func tipTapped(sender: AnyObject) {
         self.view.endEditing(true)
+        calculateTip()
     }
     
     override func didReceiveMemoryWarning() {
@@ -111,6 +94,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         self.view.endEditing(true)
+        calculateTip()
     }
 
 }
